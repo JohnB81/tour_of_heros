d2a73be6c40e15370bd7a033bd11ee744b038930
INBOUND\jbair <jbair@inboundcs.com>
2018-03-25 13:14:31 -0600

    attempt to implement step 3 - The Hero Editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
